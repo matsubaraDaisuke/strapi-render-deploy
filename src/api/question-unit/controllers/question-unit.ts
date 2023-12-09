e330212119dd6f58da38e780464b14dee480f92b
matsubaraDaisuke <matsunenenkao@gmail.com>
2023-12-09 23:56:04 +0900

    add:question-unit

---
+++ src/api/question-unit/controllers/question-unit.ts
@@ -0,0 +1,7 @@
+/**
+ * question-unit controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::question-unit.question-unit');
---
+++ src/api/question-unit/services/question-unit.ts
@@ -0,0 +1,7 @@
+/**
+ * question-unit service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::question-unit.question-unit');
